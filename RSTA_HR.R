d5a8a0216df44222efa9733ffc8ca92df71c66b2
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-13 10:10:56 +0600

    comment

--- RSTA_HR.R
+++ RSTA_HR.R
@@ -17,17 +17,9 @@ data_RSTA %>%
     theme_linedraw()
 ggsave("output/staff_by_region.jpg", width = 20, height = 12, units = "cm")
 
-PL_list <- data_RSTA %>% 
-    select(PL) %>% 
-    unique() %>% 
-    pull()
-
 data_RSTA$PL <- factor(data_RSTA$PL, levels = c("EX2", "P1", "P2","P3", 
     "P4", "P5","SS1", "SS2", "SS3", "SS4", "S1", "S2", "S3", "S4", 
     "S5", "O1", "O2", "O3", "O4", "ESP", "GSP I", "GSP II"))
-levels(data_RSTA$PL)
-
-str(data_RSTA$PL)
 
 data_RSTA %>% 
     group_by(PL) %>% 
@@ -69,21 +61,21 @@ school_list <- data_RSTA %>%
     unique() %>% 
     pull()
 
+data_RSTA$new_qual <- 0
 data_RSTA <- data_RSTA %>% 
-    mutate(new_qual = ifelse(qualification %in% master_list, "Masters", new_qual)) %>% 
+    mutate(new_qual = ifelse(qualification %in% master_list,
+                             "Masters", new_qual)) %>% 
     mutate(new_qual = ifelse(qualification %in% bachelor_list, "Bachelors", new_qual)) %>% 
-    mutate(new_qual = ifelse(qualification %in% diploma, "Diploma", new_qual)) %>% 
+    mutate(new_qual = ifelse(qualification %in% diploma,
+                             "Diploma", new_qual)) %>% 
     mutate(new_qual = ifelse(qualification %in% certificate, "Certificate", new_qual)) %>% 
-    mutate(new_qual = ifelse(qualification %in% school_list, "School", new_qual))
+    mutate(new_qual = ifelse(qualification %in% school_list, 
+                             "School", new_qual))
 
 data_RSTA$new_qual <- factor(data_RSTA$new_qual,
     levels = c("Masters", "Bachelors", "Diploma", "Certificate", "School"))
 
 fct_count(data_RSTA$new_qual)
-
-data_RSTA %>% 
-    filter(is.na(new_qual)) %>% 
-    View()
 
 data_RSTA %>% 
     group_by(new_qual) %>% 
@@ -93,3 +85,5 @@ data_RSTA %>%
     coord_flip() +
     ylim(0, 80) +
     theme_linedraw()
+
+WriteXLS::WriteXLS(data_RSTA, "RSTA_staff.xlsx")
